9954b7a00e31f6f89ac1490cfea6e8e507f02b63
Vinicius Pinheiro <vinicius.pinheiro@wisereducacao.com>
2021-01-15 14:03:21 -0300

    feat: add project

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Netxjs Typescript</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Welcome to Typescript NextJs
+        </h1>
+      </main>
+    </div>
+  );
+}
